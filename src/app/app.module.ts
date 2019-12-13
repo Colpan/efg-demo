cda8e39d5f4ffbfc5dd5090539fac1a5ba991446
Mark <mark.uy@justworksdevelopment.com>
2019-12-13 11:23:14 +0700

    refactor(appModule): add country module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,6 +25,7 @@ import { LanguageModule } from './language/language.module';
 import { RegisterModule } from './register/register.module';
 import {TranslateLoader} from '@ngx-translate/core';
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import { CountryModule } from './country/country.module';
 
 const appRoutes: Routes = [
     {
@@ -79,6 +80,7 @@ const appRoutes: Routes = [
         AppStoreModule,
         LanguageModule,
         RegisterModule,
+        CountryModule,
     ],
     bootstrap   : [
         AppComponent
